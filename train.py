45e4f58ad5ec05b8f757e06ea563a0a289036bf1
Ichigo <ichigo@kunana.ai>
2021-10-19 21:53:35 +0700

    fix implementation.

--- train.py
+++ train.py
@@ -1,22 +1,27 @@
-from tokenizers.trainers import WordPieceTrainer
-from tokenizers import Tokenizer, SentencePieceBPETokenizer
+from tokenizers.trainers import BpeTrainer, WordPieceTrainer
+from tokenizers import Tokenizer
 from tokenizers.models import WordPiece, BPE
 from tokenizers.implementations import SentencePieceBPETokenizer
 from glob import glob
 
 
-def main(tokenizer_type='sentencepiece_bpe'):
+def main(tokenizer_type='sentencepiece_bpe', vocab_size=9000):
     files = glob('*.txt')
     print('tokenize', files)
+    trainer_kwargs = {'special_tokens': ["[UNK]", "[CLS]", "[SEP]", "[PAD]", "[MASK]"], 'vocab_size': vocab_size, 'min_frequency':0}
+
     if tokenizer_type == 'sentencepiece_bpe':
         tokenizer = SentencePieceBPETokenizer(dropout=0.9999)
+        tokenizer.train(files=files, **trainer_kwargs)
     elif tokenizer_type == 'bpe':
         tokenizer = Tokenizer(BPE(dropout=0.9999))
+        trainer = BpeTrainer(files=files, **trainer_kwargs)
+        tokenizer.train(files=files, trainer=trainer)
     elif tokenizer_type == 'wordpiece':
         tokenizer = Tokenizer(WordPiece())
-    trainer = WordPieceTrainer(special_tokens=["[UNK]", "[CLS]", "[SEP]", "[PAD]", "[MASK]"], vocab_size=9000, min_frequency=0)
-    tokenizer.train(files=files, trainer=trainer)
-    tokenizer.save("tokenizer-wiki.json")
+        trainer = WordPieceTrainer(**trainer_kwargs)
+        tokenizer.train(files=files, trainer=trainer)
+    tokenizer.save(f"tokenizer-{tokenizer_type}.json")
 
 if __name__ == '__main__':
     main()
